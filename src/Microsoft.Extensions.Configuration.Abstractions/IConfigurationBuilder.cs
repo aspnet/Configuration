78df3a0a7fb22d3f640b75dc3e2d591dcc4900f7
damianedwards <damian@damianedwards.com>
2015-10-20 22:42:59 -0700

    Added doc-comments for main API - Coherence-Signed#75

--- src/Microsoft.Extensions.Configuration.Abstractions/IConfigurationBuilder.cs
+++ src/Microsoft.Extensions.Configuration.Abstractions/IConfigurationBuilder.cs
@@ -5,14 +5,33 @@ using System.Collections.Generic;
 
 namespace Microsoft.Extensions.Configuration
 {
+    /// <summary>
+    /// Represents a type used to build application configuration.
+    /// </summary>
     public interface IConfigurationBuilder
     {
+        /// <summary>
+        /// TODO: Not really sure what this is...
+        /// </summary>
         Dictionary<string, object> Properties { get; }
 
+        /// <summary>
+        /// Returns the providers used to obtain configuation values.
+        /// </summary>
         IEnumerable<IConfigurationProvider> Providers { get; }
 
+        /// <summary>
+        /// Adds a new configuration provider.
+        /// </summary>
+        /// <param name="provider">The configuration provider to add.</param>
+        /// <returns>The same configuration provider.</returns>
         IConfigurationBuilder Add(IConfigurationProvider provider);
 
+        /// <summary>
+        /// Builds an <see cref="IConfiguration"/> with keys and values from the set of providers registered in
+        /// <see cref="Providers"/>.
+        /// </summary>
+        /// <returns>An <see cref="IConfigurationRoot"/> with keys and values from the registered providers.</returns>
         IConfigurationRoot Build();
     }
 }
