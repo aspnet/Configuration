d9824e6b0d709798d4afa7972e69a56a5bf38233
Pawel Grudzien <p.grudzien12@gmail.com>
2015-11-17 22:10:25 +0100

    bug fix #335

--- test/Microsoft.Extensions.Configuration.Json.Test/JsonConfigurationTest.cs
+++ test/Microsoft.Extensions.Configuration.Json.Test/JsonConfigurationTest.cs
@@ -138,23 +138,12 @@ namespace Microsoft.Extensions.Configuration
         [Fact]
         public void ThrowFormatExceptionWhenFileIsEmpty()
         {
-            //var config = new ConfigurationBuilder();
-            //var tempFileName = "C:\\temp\\" + Guid.NewGuid().ToString() + ".json";
-            //var tempFile = File.Create(tempFileName);
-            //tempFile.Close();
-            //config.AddJsonFile(tempFileName);
-
-            //var exception = Assert.Throws<FormatException>(
-            //    () => config.AddJsonFile(tempFileName));
-            //Assert.NotNull(exception.Message);
-
             var json = @"";
             var jsonConfigSource = new JsonConfigurationProvider("EmptyFile.txt");
 
             var exception = Assert.Throws<FormatException>(
                 () => jsonConfigSource.Load(TestStreamHelpers.StringToStream(json)));
-            throw exception;
-            //Assert.NotNull(exception.Message);
+            Assert.NotNull(exception.Message);
         }
     }
 }
