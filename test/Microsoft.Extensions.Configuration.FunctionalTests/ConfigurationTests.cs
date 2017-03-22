1e019796bdbe4cba6d43b2d57b757aed7f8b6471
Jass Bagga <jbagga93@live.ca>
2017-03-22 12:44:55 -0700

    Skip Linux

--- test/Microsoft.Extensions.Configuration.FunctionalTests/ConfigurationTests.cs
+++ test/Microsoft.Extensions.Configuration.FunctionalTests/ConfigurationTests.cs
@@ -682,6 +682,7 @@ IniKey1=IniValue2");
 
         [ConditionalTheory(Skip = "https://github.com/aspnet/Configuration/issues/628")]
         [OSSkipCondition(OperatingSystems.MacOSX)]
+        [OSSkipCondition(OperatingSystems.Linux)]
         [InlineData(false)]
         [InlineData(true)]
         public async Task DeletingFileWillReload(bool optional)
