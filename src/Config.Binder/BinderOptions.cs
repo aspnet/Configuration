b3f5ebb663ce8401c81b09049899b561a40ed513
Hao Kung <haok@microsoft.com>
2018-03-12 14:05:54 -0700

    Add BinderOptions + non public flag

---
+++ src/Config.Binder/BinderOptions.cs
@@ -0,0 +1,25 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.Configuration.Binder;
+
+namespace Microsoft.Extensions.Configuration
+{
+    /// <summary>
+    /// Options class used by the <see cref="ConfigurationBinder"/>.
+    /// </summary>
+    public class BinderOptions
+    {
+        /// <summary>
+        /// When false (the default), the binder will only attempt to set public properties.
+        /// If true, the binder will attempt to set all non read-only properties.
+        /// </summary>
+        public bool BindNonPublicProperties { get; set; }
+
+    }
+}
